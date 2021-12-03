1b693789989c5b96b5c1e443ac08ba979bf16b28
XuanRanDev <3135419729@qq.com>
2021-12-03 23:01:07 +0800

    Finish P1424

--- P1424小鱼的航程.py
+++ P1424小鱼的航程.py
@@ -1,5 +1,13 @@
 if __name__ == '__main__':
-    x = input()
-    n = input()
+    E = 250
+    data = input().split()
+    d = int(data[0])
+    day = int(data[1])
+    ans = 0
 
-    print(x , n)
+    for i in range(d, d + day):
+        if 1 <= i % 7 <= 5:
+            ans = ans + E
+
+
+    print(ans)
